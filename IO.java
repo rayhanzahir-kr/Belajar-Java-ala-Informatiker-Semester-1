ede88394b6e143f8546b94e4f8b1be7a909d8c54
Rayhan Zahir <rayhanzahir.kr@gmail.com>
2025-11-23 22:57:34 +0100

    Adding The Projett Date Counter

---
+++ IO.java
@@ -0,0 +1,9 @@
+public class IO {
+    public static void print(String s) {
+        System.out.print(s);
+    }
+
+    public static void println(String s) {
+        System.out.println(s);
+    }
+}
